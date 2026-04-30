211063ef90c1766fbdd9730dd9b796cd429007c5
Manu Villaverde <manuvillaverde2@gmail.com>
2026-04-30 16:32:46 -0300

    New enemy Asset and PlayerHealth script (unfinished)

---
+++ Assets/Script/PlayerHealth.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public float maxHealth = 100f;
+    private float _currentHealth;
+    public Slider healthSlider;
+
+    void Start()
+    {
+        _currentHealth = maxHealth;
+
+        if (healthSlider != null)
+        {
+            healthSlider.maxValue = maxHealth;
+            healthSlider.value = _currentHealth;
+        }
+    }
+    void Update()
+    {
+        if (healthSlider != null)
+            healthSlider.value = _currentHealth;
+    }
+    public void TakeDamage(float damage)
+
+    {
+        _currentHealth -= damage;  
+        _currentHealth = Mathf.Clamp(_currentHealth, 0, maxHealth);
+
+        Debug.Log("Vida actual:" +  _currentHealth);
+
+        if (_currentHealth <= 0)
+        {
+            Die();
+        }
+    }
+
+    private void Die()
+    {
+        Debug.Log("El jugador murió");
+
+
+    }
+}
